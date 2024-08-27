dd31cf17471e793b75251eaa5a31c17a03b88ee4
Sergei Fladung <sergeifladung@web.de>
2024-08-28 00:00:57 +0200

    Initial commit 5

--- Calcusino/Program.cs
+++ Calcusino/Program.cs
@@ -23,4 +23,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-// DELETEME-comment for testing initial GIT-Commit, -Push, -Pull und -Brancheses
+// DELETEME-comment for testing initial GIT-Commit, -Push, -Pull und -Brancheseses
