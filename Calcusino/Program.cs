83776c83b39f5dfbe1e643de4a9d1903208bd092
Napfkuchen <sergeifladung@googlemail.com>
2024-08-27 23:48:49 +0200

    Update Program.cs
    Initial comment 3

--- Calcusino/Program.cs
+++ Calcusino/Program.cs
@@ -23,4 +23,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-// DELETEME-comment for testing initial GIT-Commit, -Push, -Pull und -Brancha
+// DELETEME-comment for testing initial GIT-Commit, -Push, -Pull und -Branches
