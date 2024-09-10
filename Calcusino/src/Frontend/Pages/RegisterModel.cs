4c74d63fd9e4f20c4e619112993aa295d27d04a8
Sergei Fladung <sergeifladung@web.de>
2024-09-11 01:18:38 +0200

    Added more input-fields to Register-Page

--- Calcusino/src/Frontend/Pages/RegisterModel.cs
+++ Calcusino/src/Frontend/Pages/RegisterModel.cs
@@ -1,6 +1,31 @@
-﻿namespace Calcusino.src.Frontend.Pages
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace Calcusino.src.Frontend.Pages
 {
-    public class RegisterModel
+    public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (Username == "admin" && Password == "password")
+            {
+                return RedirectToPage("/Index"); // Successfull Login
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return Page(); // Error on Login
+        }
+
+        public IActionResult OnPostRegister()
+        {
+            return RedirectToPage("/Register"); // Forwarding to Registration page
+        }
     }
 }
